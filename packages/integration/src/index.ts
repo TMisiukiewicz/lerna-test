026de7a8fa522759b49dc5947f40c8703bc6f2c7
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-02-19 16:54:06 +0100

    setup lerna

---
+++ packages/integration/src/index.ts
@@ -0,0 +1,7 @@
+import { Doctor } from "@lerna-test/doctor";
+import { Patient } from "@lerna-test/patient";
+import { Scheduler } from "@lerna-test/scheduler";
+
+console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
